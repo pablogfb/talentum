c028d25a8e6b382955e906d347875646c08ed643
pablogfb <pablobuntu@gmail.com>
2016-01-28 07:50:50 +0100

    Nueva version de Card.rb

--- src2/Prohis/model/Card.rb
+++ src2/Prohis/model/Card.rb
@@ -1,3 +1,17 @@
 class Card
-  
+  def initialize legal
+    @legal=legal
+    case @legal
+    when true
+      @value=1000
+    when false
+      @value=4000
+    when "Teniente"
+      @value=3000
+    when "Capitan"
+      @value=4000
+    when "Inspector"
+      @value=5000
+    end
+  end
 end
